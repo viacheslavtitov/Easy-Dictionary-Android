a61c5860b73763b3a60157c0f16287389dbfe938
Viacheslav Titov <viacheslavtitov90@gmail.com>
2023-04-21 10:46:25 +0300

    added data layer

---
+++ app/src/main/java/my/dictionary/free/domain/usecases/users/GetUpdateUsersUseCase.kt
@@ -0,0 +1,21 @@
+package my.dictionary.free.domain.usecases.users
+
+import my.dictionary.free.data.models.users.UsersTable
+import my.dictionary.free.data.repositories.DatabaseRepository
+import my.dictionary.free.domain.models.users.User
+
+class GetUpdateUsersUseCase(private val databaseRepository: DatabaseRepository) {
+
+    suspend fun insertOrUpdateUser(user: User): Boolean {
+        return databaseRepository.insertOrUpdateUser(
+            user = UsersTable(
+                _id = user._id,
+                name = user.name,
+                email = user.email,
+                providerId = user.providerId,
+                uid = user.uid
+            )
+        )
+    }
+
+}
